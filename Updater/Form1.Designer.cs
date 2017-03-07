decde95eb89d1ee94e121308d38fcbebf139f7ae
lockhj <joseph.lockhart@astrodynetdi.com>
2017-03-07 15:33:37 -0500

    First commit of dll updater

--- Updater/Form1.Designer.cs
+++ Updater/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(530, 20);
+            this.textBox1.TabIndex = 0;
+           // this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 56);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(530, 94);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "lblUpdatingAppName";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(578, 185);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "UPDATER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Updater/Form1.cs
+++ Updater/Form1.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +13,262 @@ using System.Windows.Forms;
 
 namespace Updater
 {
+
+
     public partial class Form1 : Form
     {
+        static string ConnStr = "Data Source=etrav-hack;Initial Catalog=images;Persist Security Info=True;User ID=Application;Password=noitacilppa";
+
+        
+
         public Form1()
+
         {
             InitializeComponent();
+            //CallingApp = null;
+            //tester
+            //getupdateApp("QualityPlan", "QualityPlanNET.frx", null);
+
+        }
+
+        public class updater : IDisposable
+        {
+            string CallingApp = null; //Fortesting
+            string CallingFormfrx = null;
+            FileStream DevEXlib = null; //Holds collection of DevExpress dll's
+            string DVXName;
+            public Process[] processes { get; private set; }
+            public void Dispose()
+            {
+                if (DevEXlib != null)
+                {
+                    DevEXlib.Dispose();
+                    DevEXlib = null;
+                }
+            }
+            public void getupdateApp(string CallingAppN, string CallingformN, string CallingVerN) //Goes and gets Updated application and Fast Report frx files as well as Devexpress dll's
+            {
+                //textBox1.Text = CallingAppN + CallingformN + CallingVerN;
+                CallingApp = CallingAppN; CallingFormfrx = CallingformN;
+                System.IO.FileStream fP;                          // Writes the BLOB to a file (*.bmp).
+                BinaryWriter bw;                        // Streams the BLOB to the FileStream object.
+                const int bufferSize = 1000;                   // Size of the BLOB buffer.
+                byte[] outbyte = new byte[bufferSize];  // The BLOB byte[] buffer to be filled by GetBytes.
+                long retval;                            // The bytes returned from GetBytes.
+                long startIndex = 0;                    // The starting position in the BLOB output.
+                closerunningapp(CallingApp);
+
+                //This section updates the CallingApp if not null
+                if (CallingApp == null) { MessageBox.Show("dll not called properly! App name missing!"); return; }
+                using (System.Data.SqlClient.SqlConnection SqlConn = new SqlConnection(ConnStr))
+                using (SqlCommand command = SqlConn.CreateCommand())
+                {
+                    //varPathToNewLocation = userRoot;
+                    SqlConn.Open();
+                    command.Parameters.AddWithValue("@varID", CallingApp);
+                    command.CommandText = string.Format("SELECT VSApplication FROM tblVSapplications WHERE Applicationname='{0}'", CallingApp + ".exe");
+                    using (SqlDataReader sqlQueryResult = command.ExecuteReader(CommandBehavior.SequentialAccess))
+
+                        while (sqlQueryResult.Read())
+                        {
+                            //fP = new FileStream(sqlQueryResult.ToString(), FileMode.OpenOrCreate, FileAccess.Write);
+                            try
+                            {
+
+
+                                fP = new FileStream(string.Format("C:\\Sql\\{0}.exe", CallingApp), FileMode.OpenOrCreate, FileAccess.Write);
+                            }
+                            catch (IOException)
+                            {
+
+                                return;
+                            }
+                            bw = new BinaryWriter(fP);
+                            startIndex = 0;
+                            retval = sqlQueryResult.GetBytes(0, startIndex, outbyte, 0, bufferSize);
+                            // Continue reading and writing while there are bytes beyond the size of the buffer.
+                            while (retval == bufferSize)
+                            {
+                                bw.Write(outbyte);
+                                bw.Flush();
+
+                                // Reposition the start index to the end of the last buffer and fill the buffer.
+                                startIndex += bufferSize;
+                                retval = sqlQueryResult.GetBytes(0, startIndex, outbyte, 0, bufferSize);
+                            }
+
+                            // Write the remaining buffer.
+                            bw.Write(outbyte, 0, (int)retval);
+                            bw.Flush();
+
+                            // Close the output file.
+                            bw.Close();
+                            fP.Close();
+                            //checkedListBox1.SetItemChecked(0, true);
+
+
+
+                        }
+
+                    reopenrunningapp(CallingApp);
+
+                    //This section updates the CallingFormfrx if not null
+                    if (CallingFormfrx == null) { MessageBox.Show("dll not called properly! frx name missing!"); return; }
+                    using (System.Data.SqlClient.SqlConnection SqlConn2 = new SqlConnection(ConnStr))
+                    using (SqlCommand command2 = SqlConn2.CreateCommand())
+                    {
+                        //varPathToNewLocation = userRoot;
+                        SqlConn.Open();
+                        command2.Parameters.AddWithValue("@varID", CallingFormfrx);
+                        command2.CommandText = string.Format("SELECT VSApplication FROM tblVSapplications WHERE Applicationname='{0}'", CallingFormfrx);
+                        using (SqlDataReader sqlQueryResult = command2.ExecuteReader(CommandBehavior.SequentialAccess))
+
+                            while (sqlQueryResult.Read())
+                            {
+                                //fP = new FileStream(sqlQueryResult.ToString(), FileMode.OpenOrCreate, FileAccess.Write);
+                                try
+                                {
+
+
+                                    fP = new FileStream("C:\\Sql\\" + CallingFormfrx, FileMode.OpenOrCreate, FileAccess.Write);
+                                }
+                                catch (IOException)
+                                {
+
+                                    return;
+                                }
+                                bw = new BinaryWriter(fP);
+                                startIndex = 0;
+                                retval = sqlQueryResult.GetBytes(0, startIndex, outbyte, 0, bufferSize);
+                                // Continue reading and writing while there are bytes beyond the size of the buffer.
+                                while (retval == bufferSize)
+                                {
+                                    bw.Write(outbyte);
+                                    bw.Flush();
+
+                                    // Reposition the start index to the end of the last buffer and fill the buffer.
+                                    startIndex += bufferSize;
+                                    retval = sqlQueryResult.GetBytes(0, startIndex, outbyte, 0, bufferSize);
+                                }
+
+                                // Write the remaining buffer.
+                                bw.Write(outbyte, 0, (int)retval);
+                                bw.Flush();
+
+                                // Close the output file.
+                                bw.Close();
+                                fP.Close();
+                                //checkedListBox1.SetItemChecked(1, true);
+
+
+                            }
+                    }
+
+                    //This section updates the DevExpress Library if not null and requested by IT.
+                    if (DevEXlib == null) { return; }
+                    //Must Delete Old DevExpress Library
+                    using (System.Data.SqlClient.SqlConnection SqlConn3 = new SqlConnection(ConnStr))
+                    using (SqlCommand command3 = SqlConn3.CreateCommand())
+                    {
+                        //varPathToNewLocation = userRoot;
+                        SqlConn.Open();
+                        command3.Parameters.AddWithValue("@varID", CallingFormfrx);
+                        command3.CommandText = string.Format("SELECT VSApplication FROM tblVSapplications WHERE Applicationname='{0}'", DVXName);
+                        using (SqlDataReader sqlQueryResult = command3.ExecuteReader(CommandBehavior.SequentialAccess))
+
+                            while (sqlQueryResult.Read())
+                            {
+                                //fP = new FileStream(sqlQueryResult.ToString(), FileMode.OpenOrCreate, FileAccess.Write);
+                                try
+                                {
+
+
+                                    fP = new FileStream("C:\\Sql\\" + DVXName, FileMode.OpenOrCreate, FileAccess.Write);
+                                }
+                                catch (IOException)
+                                {
+
+                                    return;
+                                }
+                                bw = new BinaryWriter(fP);
+                                startIndex = 0;
+                                retval = sqlQueryResult.GetBytes(0, startIndex, outbyte, 0, bufferSize);
+                                // Continue reading and writing while there are bytes beyond the size of the buffer.
+                                while (retval == bufferSize)
+                                {
+                                    bw.Write(outbyte);
+                                    bw.Flush();
+
+                                    // Reposition the start index to the end of the last buffer and fill the buffer.
+                                    startIndex += bufferSize;
+                                    retval = sqlQueryResult.GetBytes(0, startIndex, outbyte, 0, bufferSize);
+                                }
+
+                                // Write the remaining buffer.
+                                bw.Write(outbyte, 0, (int)retval);
+                                bw.Flush();
+
+                                // Close the output file.
+                                bw.Close();
+                                fP.Close();
+                                //checkedListBox1.SetItemChecked(2, true);
+                                //unpack zip
+                            }
+                    }
+                }
+                //this.Close();
+                return;
+            }
+
+            private void reopenrunningapp(string callingApp)
+            {
+                var p = new Process();
+                p.StartInfo.FileName = callingApp + ".exe";
+                p.Start();
+            }
+
+            public void closerunningapp(string app)
+            {
+                app = app.ToLower();
+                //Process myprc = GetaProcess(app); myprc.Kill();
+                Process[] processes = Process.GetProcesses();
+                try
+                {
+                    foreach (Process proc in processes)
+                    {
+                        string ProcessName = proc.ProcessName;
+                        ProcessName = ProcessName.ToLower();
+                        if (ProcessName.CompareTo(app) == 0)
+                            proc.Kill();
+                        //proc.CloseMainWindow();
+                        //proc.WaitForExit();
+                    }
+                    //buildList();
+                }
+                catch (System.NullReferenceException)
+                {
+                    //MessageBox.Show("No instances of" + app + "running.");
+                    //checkedListBox1.Items.Add(string.Format("No instances of{0}running.", app));
+                    //checkedListBox1.Update();
+                }
+            }
+
+            private Process GetaProcess(string processname)
+            {
+                Process[] aProc = Process.GetProcessesByName(processname);
+
+                if (aProc.Length > 0)
+                    return aProc[0];
+
+                else return null;
+            }
+
+            void myprc_Exited(object sender, EventArgs e)
+            {
+                MessageBox.Show(((Process)sender).ProcessName + " process has exited!");
+            }
+
         }
     }
+
 }
